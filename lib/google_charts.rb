b07946fd3db6cb8b6658c2e1a3fa7f0292ab0c56
Matthew Schulkind <mschulkind@gmail.com>
2013-01-30 12:25:11 -0500

    Add default point size.

--- lib/google_charts.rb
+++ lib/google_charts.rb
@@ -6,9 +6,11 @@ module GoogleCharts
     def self.define( name, options = {} )
       helper_name = [options[:prefix], name, options[:suffix]||'chart'].compact.join( '_' )
 
+      default_chart_options = options[:defaults] || {}
+
       GoogleCharts::Helpers.module_eval <<-DEF
-        def #{helper_name}( *options )
-          chart = GoogleCharts::Charts::#{name.to_s.capitalize}.new( self, *options )
+        def #{helper_name}( data, options={} )
+          chart = GoogleCharts::Charts::#{name.to_s.capitalize}.new( self, data, #{default_chart_options}.merge(options) )
           yield chart if block_given?
 
           chart.to_html
@@ -16,9 +18,9 @@ module GoogleCharts
       DEF
     end
 
-    define :line
+    define :line, defaults: {pointSize: 4}
     define :pie
-    define :area
+    define :area, defaults: {pointSize: 4}
     define :bar
     define :column
     define :geo
